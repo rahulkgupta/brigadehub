38c5c5355a2dae9844bab7b410bd431f2c552ad1
dbcjckyee <dbcjckyee@gmail.com>
2016-01-31 17:55:57 -0800

    add test for slack frame. create separate tests for mainpage, slack page, and mail list page. add beforeeach function to start each test on the mainpage

--- test/nightwatch/mainpageatl.js
+++ test/nightwatch/mainpageatl.js
@@ -1,9 +1,20 @@
 module.exports = {
+  beforeEach : function(browser) {
+    browser
+      .windowHandles(function(result){
+        var homepage = result.value[0]
+        browser.switchWindow(homepage)
+      })
+  },
   'Demo atl theme CFA' : function (browser) {
     browser
       .url('localhost:5465')
       .waitForElementVisible('body', 1000)
       .assert.containsText('.lead', "We're a bunch of civic-minded technologists, designers, and topic experts using our skills to improve Example and the world")
+
+  },
+  'Mail list signup test' : function(browser){
+    browser
       .click("#mc-embedded-subscribe")
       .windowHandles(function(result) {
         browser.assert.equal(result.value.length, 2, 'There should be two windows open.');
@@ -12,13 +23,21 @@ module.exports = {
       })
       .waitForElementVisible('#subscribeFormWelcome', 5000)
       .assert.containsText('.masthead', "Website Signup")
-      .windowHandles(function(result) {
-        browser.assert.equal(result.value.length, 2, 'There should be two windows open.');
-        var mainpage = result.value[0];
+  },
+  'Slack test' : function(browser) {
+    browser
+      .waitForElementVisible('.__slackin', 5000)
+      .pause(3000)
+      .frame(0)
+        .click(".slack-btn")
+        .pause(3000)
+        .windowHandles(function(result) {
+        browser.assert.equal(result.value.length, 3, 'There should be three windows open.');
+        var mainpage = result.value[2];
         browser.switchWindow(mainpage);
       })
-      // .waitForElementVisible('body', 1000)
-      // .assert.containsText('.lead', "We're a bunch of civic-minded technologists, designers, and topic experts using our skills to improve Example and the world")
+        .assert.title('Join sfbrigade on Slack!')
+        .frame(null)
   },
 
   after : function(browser) {
