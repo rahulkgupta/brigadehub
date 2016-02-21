39100e3ddd4fe0a0a11e87671be00f949c62896f
dbcjckyee <dbcjckyee@gmail.com>
2016-02-20 17:16:15 -0800

    create first migration

--- migrations/1456016742272-testing.js
+++ migrations/1456016742272-testing.js
@@ -1,9 +1,11 @@
 'use strict'
-
+var db =require('./data/db')
 exports.up = function(next) {
+  db.insert({"Name":"test"})
   next();
 };
 
 exports.down = function(next) {
+  db.remove({"Name":"test"})
   next();
 };
