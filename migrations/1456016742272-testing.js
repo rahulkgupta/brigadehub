45487d858bc365b7746eab2fff4c9f107134faa9
Trent Oswald <trentoswald@therebelrobot.com>
2016-02-22 16:34:49 -0800

    chore(migrations): added test migration

--- migrations/1456016742272-testing.js
+++ migrations/1456016742272-testing.js
@@ -1,10 +1,62 @@
 'use strict'
-var db = require('./db');
-exports.up = function(next) {
-  db.brigadehub.insert({"Name":"testplease"})
-  next();
-};
-exports.down = function(next) {
-  db.brigadehub.remove({"Name":"test"})
-  next();
-};
+var db = require('./db')
+exports.up = function (next) {
+  // run connect promise
+  db.connect().then(function (db) {
+    console.log('connected')
+    // set up desired collection
+    var testCollection = db.collection('users')
+
+    // run mongo call on collection
+    testCollection.insertMany([{'username': 'testplease'}], function (err, result) {
+      // appropriately catch insert error
+      if (err) {
+        console.error(err)
+        throw new Error(err)
+      }
+
+      // close connection
+      db.close()
+
+      // continue migrations
+      next()
+    })
+  })
+    // Appropriately catch a connection error
+    .catch(function (err, db) {
+      console.error(err)
+      throw new Error(err)
+    })
+
+// end of up migration
+}
+exports.down = function (next) {
+  // run connect promise
+  db.connect().then(function (db) {
+    console.log('connected')
+    // set up desired collection
+    var testCollection = db.collection('users')
+
+    // run mongo call on collection
+    testCollection.deleteOne({'username': 'testplease'}, function (err, result) {
+      // appropriately catch insert error
+      if (err) {
+        console.error(err)
+        throw new Error(err)
+      }
+
+      // close connection
+      db.close()
+
+      // continue migrations
+      next()
+    })
+  })
+    // Appropriately catch a connection error
+    .catch(function (err, db) {
+      console.error(err)
+      throw new Error(err)
+    })
+
+// end of down migration
+}
--- migrations/db.js
+++ migrations/db.js
@@ -1,4 +1,40 @@
-var mongo = require('mongodb').MongoClient
-// var Server = require('mongodb').Server
-// var database = new mongo(new Server("localhost", 27017, {native_parser: true}));
-module.exports = mongo;
+var fs = require('fs')
+var path = require('path')
+var dotenv = require('dotenv')
+var MongoDB = require('mongodb').MongoClient
+
+// Grab .env variables
+try {
+  var envFilePath = path.resolve(__dirname, '../.env')
+  var stats = fs.lstatSync(envFilePath)
+  if (stats.isFile()) {
+    dotenv.load({ path: envFilePath })
+  } else {
+    throw new Error('.env is not a file!')
+  }
+} catch (e) {
+  console.warn(e)
+  console.warn('.env file not found. Defaulting to sample. Please copy .env.example to .env and populate with your own credentials.')
+  dotenv.load({ path: envFilePath+'.example' })
+}
+
+// Set up a central connection promise
+var mongoConnect = function mongoConnectDB(){
+  return new Promise(function(resolve, reject){
+    var url = process.env.MONGODB;
+    // Use connect method to connect to the Server
+    MongoDB.connect(url, function(err, db) {
+      if(err) {
+        console.error(err)
+        return reject(err, db)
+      }
+      console.log("Connected to MongoDB database:", url);
+      resolve(db)
+    });
+  })
+}
+
+module.exports = {
+  // allow for more db actions
+  connect:mongoConnect
+}
